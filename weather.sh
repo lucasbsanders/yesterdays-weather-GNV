31b7e6839766a7689662be8e75eeb719b7c15c35
lucassanders <lucasbsanders@ufl.edu>
2016-09-26 10:50:21 -0400

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo Hello World
+year=`date -d yesterday +%Y`
+month=`date -d yesterday +%m`
+day=`date -d yesterday +%d`
+
+curl -s "https://www.wunderground.com/history/airport/KGNV/$year/$month/$day/Da$
+
+max=$(cat gnvweather.txt | cut -f2 -d"," | sort -nr | head -n1)
+
+echo "Yesterday's high was: $max"
